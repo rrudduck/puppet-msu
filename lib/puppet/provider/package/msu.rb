e7bdfbc0d4a96dd65bd1c51029c525164e568b01
Robert Rudduck <robert@rpowered.net>
2014-01-19 21:36:35 -0600

    Updated tests and added a better implementation to get currently installed packages.

--- lib/puppet/provider/package/msu.rb
+++ lib/puppet/provider/package/msu.rb
@@ -1,31 +1,35 @@
 require 'puppet/provider/package'
 require 'win32ole'
+require 'win32/registry'
 
 Puppet::Type.type(:package).provide :msu, :parent => Puppet::Provider::Package do
-    desc "MSU package provider for windows"
+    desc "msu package provider for windows"
+
     confine :operatingsystem => :windows
+
     has_feature :installable, :uninstallable
+
     commands :wsua => 'C:/Windows/Sysnative/wsua.exe'
 
     def print
-        notice("${name} ${path}")
+        notice("${name} ${source}")
     end
 
     def install
         # wsua.exe /quiet /norestart <msu file>
-        args = '/quiet', '/norestart', @resource[:path]
+        args = '/quiet', '/norestart', @resource[:source]
 
         wsua(*args)
     end
 
     def uninstall
         # wsua.exe /quiet /norestart /uninstall <msu file or kb>
-        args = '/quiet', '/norestart', '/uninstall'
-        
+        args = '/quiet', '/norestart', '/uninstall', '/kb'
+
         if @resource[:name].downcase.start_with? 'kb'
-            args << '/kb' << @resource[:name]
+            args << @resource[:name]
         else
-            args << @resource[:path]
+            raise Puppet::Error, 'name should start with kb'
         end
 
         wsua(*args)
@@ -33,30 +37,50 @@ Puppet::Type.type(:package).provide :msu, :parent => Puppet::Provider::Package d
 
     def query
         self.class.instances.each do |p|
-            return p.properties if @resource[:name] == p.name
+            return p.properties if @resource[:name].downcase == p.name.downcase
         end
-
         nil
     end
 
     def self.instances    
+        query_registry
+    end
 
+    # This is probably the safer method, but it is quite a bit slower
+    def self.query_wmi
         packages = []
 
-        begin
-            wmi = WIN32OLE.connect('winmgmts://')
+        wmi = WIN32OLE.connect('winmgmts://')
 
-            hotfixes = wmi.ExecQuery('select * from win32_quickfixengineering')
+        hotfixes = wmi.ExecQuery('select * from win32_quickfixengineering')
 
-            for hotfix in hotfixes do
-                packages << new({ :name => hotfix.HotFixID, :ensure => :present, :provider => self.name  })
+        hotfixes.each do |hotfix|
+            packages << new({ :name => hotfix.HotFixID.downcase, :ensure => :present, :provider => self.name  })
+        end
+    end
+
+    # Faster method, although a bit more work that using wmi (and possibly could miss a package)
+    def self.query_registry
+        packages = []
+
+        key = 'SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages'
+        access = Win32::Registry::KEY_READ | 0x100
+
+        Win32::Registry::HKEY_LOCAL_MACHINE.open(key, access) do |reg|
+            reg.each_key do |k|
+                reg.open(k, access) do |subkey|
+                    if subkey['InstallClient'] == 'WindowsUpdateAgent'
+                        k.split('~')[0].split('_').each do |p|
+                            if p.downcase.start_with? 'kb'
+                                packages << new({ :name => p, :ensure => :present, :provider => self.name  })
+                                break
+                            end
+                        end
+                    end
+                end
             end
-            
-        rescue Puppet::ExecutionFailure
-            return nil
         end
 
         packages
     end
-
 end
--- spec/msu_spec.rb
+++ spec/msu_spec.rb
@@ -1,1 +1,67 @@
 require 'spec_helper'
+
+provider = Puppet::Type.type(:package).provider(:msu)
+
+describe provider do 
+
+	before(:each) do
+	    @resource = Puppet::Type.type(:package).new(
+	      :name     => "msu",
+	      :ensure   => :present,
+	      :provider => :msu
+	    )
+
+	    @provider = provider.new(@resource)
+
+	    provider.stubs(:healthcheck)
+	end
+
+	it 'should have an install method' do
+		@provider.should respond_to(:install)
+	end
+
+	it 'should have an uninstall method' do
+		@provider.should respond_to(:uninstall)
+	end
+
+	context "parameter :source" do
+		it 'should default to nil' do
+			@resource[:source].should be_nil
+		end
+
+		it 'should accept c:\install\package.msu' do
+			@resource[:source] = 'c:\install\package.msu'
+		end
+	end
+
+	describe 'when installing' do
+		it 'should use source' do
+			@resource[:source] = 'c:\install\package.msu'
+			@resource[:ensure] = :present
+			@provider.expects(:wsua).with '/quiet', '/norestart', 'c:\install\package.msu'
+			@provider.install
+		end
+	end
+
+	describe 'when uninstalling' do
+		it 'should uninstall correctly if provided a kb number' do
+			@resource[:name] = 'kb1234'
+			@resource[:ensure] = :absent
+			@provider.expects(:wsua).with '/quiet', '/norestart', '/uninstall', '/kb', 'kb1234'
+			@provider.uninstall
+		end
+
+		it 'should throw if not provided a kb number' do
+			@resource[:name] = 'k1234'
+			@resource[:ensure] = :absent
+			expect { @provider.uninstall }.to raise_error
+		end
+	end
+
+	describe 'when fetching installed list' do
+		it 'should return all installed packages' do
+			packages = provider.instances
+			packages.should_not be_empty
+		end
+	end
+end
