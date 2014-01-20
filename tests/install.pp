6be85fbfada3eb4a19b3aa88f0933d4bfea4975b
Robert Rudduck <robert@rpowered.net>
2014-01-20 12:45:28 -0600

    Fixed syntax error in install.pp

--- tests/install.pp
+++ tests/install.pp
@@ -1,6 +1,6 @@
 # tests install using msu provider
 package { 'KB2506143' :
 	ensure => present,
-	provider => msu
-	source => 'C:\\Windows6.1-KB2506143-x64.msu'
+	provider => msu,
+	source => 'C:\\Windows6.1-KB2506143-x64.msu',
 }
